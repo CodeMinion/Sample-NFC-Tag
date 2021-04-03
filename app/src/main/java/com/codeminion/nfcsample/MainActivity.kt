a4fa3a9241ec321946f7ffff44ecd1e2834eb818
Frank E. Hernandez <hernandez.frank@gmail.com>
2021-04-02 20:54:08 -0400

    Updated to write any message.

--- app/src/main/java/com/codeminion/nfcsample/MainActivity.kt
+++ app/src/main/java/com/codeminion/nfcsample/MainActivity.kt
@@ -1,16 +1,22 @@
 package com.codeminion.nfcsample
 
 import android.content.Intent
+import android.nfc.NdefMessage
+import android.nfc.NdefRecord
 import android.nfc.NfcAdapter
 import android.nfc.Tag
 import android.nfc.tech.MifareUltralight
+import android.nfc.tech.NdefFormatable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.TextView
 import java.nio.charset.Charset
 
 class MainActivity : AppCompatActivity() {
+
+    private val MIFARE_CHUNK_SIZE_BYTES = 4
 
     var mDisplayField:TextView? = null
     var mWriteButton: View? = null
@@ -24,7 +30,8 @@ class MainActivity : AppCompatActivity() {
 
         mWriteButton?.setOnClickListener {
             mFoundTag?.let { tag->
-                writeTag(tag)
+                //writeTag(tag = tag, message = "No Data")
+                writeTag(tag = tag, message = "Hello Brother!")
             }
         }
 
@@ -33,25 +40,42 @@ class MainActivity : AppCompatActivity() {
     override fun onNewIntent(intent: Intent) {
         super.onNewIntent(intent)
         if (NfcAdapter.ACTION_TAG_DISCOVERED == intent.action) {
-
-
             intent.getParcelableExtra<Tag>(NfcAdapter.EXTRA_TAG)?.also {
                 mFoundTag = it
                 val tagText = readTag(it)
                 mDisplayField?.text = tagText
             }
-
         }
+
+     }
+
+    fun formatMsgForMifare(msg:String ):List<String> {
+        val leftOver = msg.length % MIFARE_CHUNK_SIZE_BYTES
+        var outMsg = msg
+        if (leftOver != 0) {
+            outMsg = msg.padEnd((msg.length / MIFARE_CHUNK_SIZE_BYTES + 1) * MIFARE_CHUNK_SIZE_BYTES, ' ')
+        }
+        return outMsg.chunked(MIFARE_CHUNK_SIZE_BYTES)
     }
 
-    fun writeTag(tag: Tag) {
+    fun writeTag(tag: Tag, message:String) {
         MifareUltralight.get(tag)?.use { ultralight ->
             ultralight.connect()
+
             Charset.forName("US-ASCII").also { usAscii ->
-                ultralight.writePage(4, "Hello".toByteArray(usAscii))
-                ultralight.writePage(5, " Bro".toByteArray(usAscii))
-                ultralight.writePage(6, "ther".toByteArray(usAscii))
-                ultralight.writePage(7, "!!!!".toByteArray(usAscii))
+                // Clear anything that might have been written.
+                // MIFARE Ultralight has 36 pages of user data.
+                for (i in 0.until(36)) {
+                    ultralight.writePage(4 + i, byteArrayOf(0,0,0,0))
+                }
+
+                val chunks = formatMsgForMifare(message)
+                var offset = 0
+                for (chunk in chunks) {
+                    // Each page in the MIFARE is 4 bytes long.
+                    ultralight.writePage(MIFARE_CHUNK_SIZE_BYTES + offset, chunk.toByteArray(usAscii))
+                    offset +=1
+                }
             }
         }
     }
